06808c513ed46edad8b824f5af5c2c2705f9f1c4
Maohe Jiang <mojiang@usc.edu>
2024-04-16 20:21:04 -0700

    completed string hash function

--- hash.h
+++ hash.h
@@ -20,15 +20,34 @@ struct MyStringHash {
     HASH_INDEX_T operator()(const std::string& k) const
     {
         // Add your code here
-
-
+        unsigned long long w[5] = {0};
+        size_t idx = 4;
+        
+        for(int i = (k.size() - 1); i >= 0; i-=6){
+            unsigned long long factor = 1;
+            for(int j = i; j > i - 6 && j >= 0; j --){
+                int c = letterDigitToNumber(k[j]);
+                w[idx] += c * factor;
+                factor *= 36;
+            }
+            idx --;
+        }
+        unsigned long long h = 0;
+        for(int i = 0; i < 5; i ++)
+            h += rValues[i] * w[i];
+        
+        return h;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
         // Add code here or delete this helper function if you do not want it
+        if(letter >= 'A' && letter <= 'Z'){
+            letter += 32;
+        }
 
+        return (letter >= 'a' && letter <= 'z') ? letter - 97 : letter - 22;
     }
 
     // Code to generate the random R values
